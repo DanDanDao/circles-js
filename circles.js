4e3e20502365c01f52b2d741b5dc501a99cb91b4
Daniel Dao <guangdanny@gmail.com>
2018-11-09 16:02:02 +1100

    add color ui

--- circles.js
+++ circles.js
@@ -2,7 +2,7 @@ window.onload = function () {
     var canvas = document.getElementById('canvas');
     c = canvas.getContext('2d');
 
-    var innerWidth = window.innerWidth - 20,
+    var innerWidth = window.innerWidth - 17,
         innerHeight = window.innerHeight - 20,
         TWO_PI = Math.PI * 2,
         circleArray = [],
@@ -21,9 +21,7 @@ window.onload = function () {
     for (var i = 0; i < params.length; i += 1) {
         var key_val = params[i].split('=');
         if (key_val.length >= 2) {
-            colorArray.splice(colorArray[4],0,"#" + key_val[1]);
-            console.log(colorArray);
-            animate();
+            colorArray.push("#" + key_val[1]);
         }
     }
 
@@ -34,14 +32,16 @@ window.onload = function () {
         btn.style.height = "40px";
         btn.style.width = "40px";
         btn.style.margin = "5px";
-        btn.style.border = "1px solid black";
+        btn.style.border = "1px solid var(--main-border-color)";
         btn.style.borderRadius = "5px";
+        btn.classList.add("color");
 
         var addedColors = document.getElementById("added-colors");
         addedColors.appendChild(btn);
         btn.onclick = function () {
-            colorArray.splice(0,1);
+            colorArray.splice(0, 1);
             console.log(colorArray);
+
         }
     }
 
@@ -75,7 +75,7 @@ window.onload = function () {
         this.draw = function () {
             c.beginPath();
             c.arc(this.x, this.y, this.radius, TWO_PI, false);
-            c.strokeStyle = '#FF63DE';
+            c.strokeStyle = 'var(--main-border-color)';
             c.fillStyle = this.color;
             c.stroke();
             c.fill();
@@ -86,7 +86,7 @@ window.onload = function () {
 // Run Circle function for creating new circle
 // Create multiple circles
     for (var i = 0; i < NumOfCircles; i++) {
-        var radius = Math.random() * 10 + 50; // Random circle size
+        var radius = Math.random() * 50; // Random circle size
         var x = Math.random() * (innerWidth - radius * 2) + radius; // Random X Position
         var y = Math.random() * (innerHeight - radius * 2) + radius; // Random Y position
         var dx = (Math.random() - 0.5) * 2; // Random X velocity
@@ -105,5 +105,4 @@ window.onload = function () {
     }
 
     animate();
-
 }
